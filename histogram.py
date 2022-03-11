6c21520ded774b81d1413a6fc75ee3067ead96b5
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2022-03-11 09:59:45 +0100

    Print percents alongside the exact count

--- histogram.py
+++ histogram.py
@@ -4,15 +4,18 @@ import sys
 
 hist = {}
 max_length = 0
+total = 0
 for line in sys.stdin:
     if line[-1] == '\n':
         line = line[:-1]
 
     hist[line] = hist.get(line, 0) + 1
     max_length = max(max_length, len(line))
+    total += 1
 
 raw = [(line, count) for line, count in hist.items()]
 raw.sort(key=lambda item: item[1], reverse=True)
 
 for line, count in raw:
-    print(f"{line:{max_length}}: {count}")
+    perc = 100.0 * (count / total)
+    print(f"{line:{max_length}}: {count} ({perc:0.2f}%)")
